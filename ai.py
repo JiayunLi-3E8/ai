d542598fa5e0f312b4c43de21e74c22340b6b372
李佳芸 <2936221683@qq.com>
2021-08-10 18:51:38 +0800

    多层，优化速度

--- ai.py
+++ ai.py
@@ -1,22 +1,25 @@
+import sys
+import math
 import numpy as np
 import pickle
+import threading
+
+from numpy.core.numeric import isclose
+from momentum import Momentum
+from matplotlib import pyplot as plt
 
 
 class Neuron:
     def __init__(self, inputNum: int):
         weights = []
         for i in range(inputNum):
-            weights.append(np.random.normal())
+            weights.append(Momentum(np.random.normal(), 0.5))
         self._weights = np.array(weights)
-        self._bias = np.random.normal()
+        self._bias = Momentum(np.random.normal(), 0.5)
 
     @staticmethod
     def _sigmoid(x) -> np.float64:
         return 1 / (1 + np.exp(-x)) if x >= 0 else np.exp(x) / (1 + np.exp(x))
-
-    @classmethod
-    def _dNormalize(cls, x, limit=1) -> np.float64:
-        return (cls._sigmoid(1 / x) - 0.5) * 2 * limit if x != 0 else limit
 
     @classmethod
     def _activation(cls, x) -> np.float64:
@@ -38,87 +41,120 @@ class Neuron:
         d = self._activation_deriv(self._basis(inputs)) * lastDerivBack
         back = d * self._weights
         for idx in range(self._weights.size):
-            #     self._weights[idx] -= learnRate * self._dNormalize(d * inputs[idx])
-            # self._bias -= learnRate * self._dNormalize(d)
-            self._weights[idx] -= learnRate * d * inputs[idx]
-        self._bias -= learnRate * d
+            self._weights[idx] << learnRate * d * inputs[idx]
+        self._bias << learnRate * d
         return back
 
 
 class NeuronNetwork:
-    def __init__(self, inputNum: int, hiddenNeuronNum: int, initLearnRate: float = 1):
-        self._learnRate = initLearnRate
-        self._hidden = []
-        for i in range(hiddenNeuronNum):
-            self._hidden.append(Neuron(inputNum))
-        self._outlet = Neuron(hiddenNeuronNum)
+    def __init__(self, inputNum: int, hiddenNeuronNum: int, hiddenLayerNum: int, initLearnRate: float = 0.5):
+        self._inputNum = inputNum
+        self._hiddenNeuronNum = hiddenNeuronNum
+        self._hiddenLayerNum = hiddenLayerNum
+        self._initLearnRate = initLearnRate
+        self._init()
 
-    @staticmethod
-    def _loss(preds: np.ndarray, trues: np.ndarray) -> np.float64:
+    def _init(self):
+        self._learnRate = self._initLearnRate
+        hiddenLayer = []
+        for i in range(self._hiddenNeuronNum):
+            hiddenLayer.append(Neuron(self._inputNum))
+        self._hiddenLayers = [hiddenLayer]
+        for n in range(self._hiddenLayerNum-1):
+            hiddenLayer = []
+            for i in range(self._hiddenNeuronNum):
+                hiddenLayer.append(Neuron(self._hiddenNeuronNum))
+            self._hiddenLayers.append(hiddenLayer)
+        self._outlet = Neuron(self._hiddenNeuronNum)
+
+    def _loss(self, datas: np.ndarray, trues: np.ndarray) -> np.float64:
+        preds = np.apply_along_axis(self.feedforward, 1, datas)
         # 均方误差（MSE）
         return ((preds - trues) ** 2).mean()
 
-    @staticmethod
-    def _loss_deriv(preds: np.ndarray, trues: np.ndarray):
-        assert preds.size == trues.size
+    def _loss_deriv(self, datas: np.ndarray, trues: np.ndarray):
+        preds = np.apply_along_axis(self.feedforward, 1, datas)
         d = []
         for idx in range(trues.size):
+            # MSE_deriv
             d.append((preds[idx] - trues[idx]) * 2 / trues.size)
         return np.array(d)
 
     def feedforward(self, inputs: np.ndarray):
-        hiddenOuts = []
-        for h in self._hidden:
-            assert isinstance(h, Neuron)
-            hiddenOuts.append(h.feedforward(inputs))
-        return self._outlet.feedforward(np.array(hiddenOuts))
+        hiddenLayerOut = []
+        for hNeuron in self._hiddenLayers[0]:
+            assert isinstance(hNeuron, Neuron)
+            hiddenLayerOut.append(hNeuron.feedforward(inputs))
+        for hiddenLayer in self._hiddenLayers[1:]:
+            hOuts = []
+            for hNeuron in hiddenLayer:
+                assert isinstance(hNeuron, Neuron)
+                hOuts.append(hNeuron.feedforward(np.array(hiddenLayerOut)))
+            hiddenLayerOut = hOuts
+        return self._outlet.feedforward(np.array(hiddenLayerOut))
+
+    def _backpropagation(self, hiddenLayer: list, backInputData: np.ndarray, transfers):
+        nextTransfers = []
+        if isinstance(transfers, np.ndarray):
+            for idx, hidNeu in enumerate(hiddenLayer):
+                assert isinstance(hidNeu, Neuron)
+                nextTransfers.append(hidNeu.backpropagation(
+                    backInputData, self._learnRate, transfers[idx]))
+        elif isinstance(transfers, list):
+            for transfer in transfers:
+                nextTransfers.append(self._backpropagation(
+                    hiddenLayer, backInputData, transfer))
+        return nextTransfers
 
     def trainOnceData(self, data: np.ndarray, dl: float):
-        hiddenOuts = []
-        for h in self._hidden:
-            assert isinstance(h, Neuron)
-            hiddenOuts.append(h.feedforward(data))
+        backInputDatas = [data]
+        hiddenLayerOut = []
+        for hNeuron in self._hiddenLayers[0]:
+            assert isinstance(hNeuron, Neuron)
+            hiddenLayerOut.append(hNeuron.feedforward(data))
+        backInputDatas.append(np.array(hiddenLayerOut))
+        for hiddenLayer in self._hiddenLayers[1:]:
+            hOuts = []
+            for hNeuron in hiddenLayer:
+                assert isinstance(hNeuron, Neuron)
+                hOuts.append(hNeuron.feedforward(np.array(hiddenLayerOut)))
+            backInputDatas.append(np.array(hOuts))
+            hiddenLayerOut = hOuts
         transfer = self._outlet.backpropagation(
-            hiddenOuts, self._learnRate, dl)
-        assert transfer.size == len(self._hidden)
-        for idx, hidNeu in enumerate(self._hidden):
-            assert isinstance(hidNeu, Neuron)
-            hidNeu.backpropagation(data, self._learnRate, transfer[idx])
+            backInputDatas[-1], self._learnRate, dl)
+        transfers = [transfer]
+        for idx, hiddenLayer in enumerate(self._hiddenLayers[::-1]):
+            transfers = self._backpropagation(
+                hiddenLayer, backInputDatas[-(idx+2)], transfers)
 
     def train(self, datas: np.ndarray, trues: np.ndarray):
-        preds = []
-        for data in datas:
-            assert isinstance(data, np.ndarray)
-            preds.append(self.feedforward(data))
-        dls = self._loss_deriv(np.array(preds), trues)
-        for idx in range(dls.size):
+        dls = self._loss_deriv(datas, trues)
+        for idx in range(len(datas)):
             self.trainOnceData(datas[idx], dls[idx])
-        preds.clear()
-        for data in datas:
-            assert isinstance(data, np.ndarray)
-            preds.append(self.feedforward(data))
-        return self._loss(np.array(preds), trues)
+        return self._loss(datas, trues)
 
-    def trainForTarget(self, datas: np.ndarray, trues: np.ndarray, TargetLoss: np.float64):
-        lastLoss = 1
+    def trainForTarget(self, datas: np.ndarray, trues: np.ndarray, TargetLoss: np.float64) -> np.float64:
+        plt.ion()
+        plt.figure(1)
+        lastLoss = 0.3
         i = 0
-        while lastLoss > TargetLoss and self._learnRate != 0:
-            i += 1
+        while lastLoss > TargetLoss:
+            lastRate = self._learnRate
             afterLoss = self.train(datas, trues)
-            if not i % 1000:
-                print("after {}\ttrain,loss {}\t,rate {}".format(
-                    i, afterLoss, self._learnRate))
-            if afterLoss - lastLoss >= 0:
-                self._learnRate /= 2
-                print("after {}\ttrain,loss {}\t,rate {}".format(
-                    i, afterLoss, self._learnRate))
-            # elif afterLoss - lastLoss > -0.000001:
-            #     self._learnRate += 0.01
-            #     print("after {}\ttrain,loss {}\t,rate {}".format(
-            #         i, afterLoss, self._learnRate))
+            i += 1
+            if math.isclose(afterLoss, lastLoss, rel_tol=1e-3):
+                self._init()
+            elif math.isclose(afterLoss, lastLoss, rel_tol=1e-2):
+                self._learnRate = 1.03 * self._learnRate
+            elif afterLoss - lastLoss > 0.1:
+                self._learnRate = self._initLearnRate
+            plt.plot((i-1, i), (lastLoss, afterLoss), color='r')
+            plt.plot((i-1, i), (lastRate, self._learnRate), color='b')
+            plt.xlabel('loss:{:<.18f}  rate:{}'.format(
+                afterLoss, self._learnRate))
+            plt.pause(sys.float_info.min)
             lastLoss = afterLoss
-        print("after {}\ttrain,loss {}\t,rate {}".format(
-            i, afterLoss, self._learnRate))
+        return lastLoss
 
 
 def main():
@@ -143,8 +179,8 @@ def main():
     try:
         objFile = open("NNB", "rb")
     except FileNotFoundError:
-        net = NeuronNetwork(2, 64)
-        net.trainForTarget(datas, all_y_trues, 0.0001)
+        net = NeuronNetwork(2, 16, 2, 0.6)
+        net.trainForTarget(datas, all_y_trues, 1e-5)
         objFile = open("NNB", "wb")
         pickle.dump(net, objFile)
         objFile.close()
@@ -152,12 +188,10 @@ def main():
         net = pickle.load(objFile)
         objFile.close()
         assert isinstance(net, NeuronNetwork)
-
-        # net._learnRate = 2
-        # net.trainForTarget(datas, all_y_trues, 0.0001)
-        # objFile = open("NNB", "wb")
-        # pickle.dump(net, objFile)
-        # objFile.close()
+        net.trainForTarget(datas, all_y_trues, 1e-8)
+        objFile = open("NNB", "wb")
+        pickle.dump(net, objFile)
+        objFile.close()
 
     while True:
         inp = eval(input("input like (a,b):"))
